e44db3f281ca0e9e398c622e7c664a6839ae2f5b
isri21 <israelalemu21@gmail.com>
2024-11-09 16:31:36 +0300

    Modified Task 1 #0

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -7,7 +7,7 @@ def list_book(request):
 	books = Book.objects.all()
 	context = {'books': books}
 
-	return render(request, 'list_books.html', context)
+	return render(request, 'relationship_app/list_books.html', context)
 
 class LibraryDetail(ListView):
 	model = Library
