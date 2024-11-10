60cf07395c6290657e4150f8df15998c76cb6131
isri21 <111064948+isri21@users.noreply.github.com>
2024-11-10 22:14:12 +0300

    Update views.py

--- django-models/relationship_app/views.py
+++ django-models/relationship_app/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 from .models import Library, Book
 from django.contrib.auth.views import LoginView, LogoutView
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 def list_books(request):
