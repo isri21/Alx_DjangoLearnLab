598babcbe81956ec08b293f7466df83b63566628
isri21 <111064948+isri21@users.noreply.github.com>
2024-11-10 22:20:57 +0300

    Update urls.py

--- django-models/relationship_app/urls.py
+++ django-models/relationship_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('library-detail', LibraryDetailView.as_view()),
     path('login/', LoginView.as_view(template_name="users/login.html")),
     path('logout/', LogoutView.as_view(template_name="users/logout.html")),
+    path('register/', views.register, name='register'),
 ]
