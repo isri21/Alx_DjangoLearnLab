43668c797dfc9e7da8a627288ee286942aefc3cb
isri21 <111064948+isri21@users.noreply.github.com>
2024-11-10 22:36:09 +0300

    Update models.py

--- django-models/relationship_app/models.py
+++ django-models/relationship_app/models.py
@@ -1,5 +1,7 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
+from django.db.models.signals import post_save 
+from django.dispatch import receiver
 # Create your models here.
 class Author(models.Model):
 	name = models.CharField(max_length=100)
@@ -40,3 +42,12 @@ class UserProfile(models.Model):
 
     def str(self):
         return self.user.username
+
+@receiver(post_save, sender=User)
+def create_user_profile(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+
+@receiver(post_save, sender=User)
+def save_user_profile(sender, instance, **kwargs):
+    instance.userprofile.save()
