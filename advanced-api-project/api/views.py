5c209db1964f96868cef34661e9ef222a8d5ae0b
isri21 <israelalemu21@gmail.com>
2024-12-01 16:33:36 +0300

    Add Task 2

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -9,6 +9,9 @@ class ListView(generics.ListAPIView):
 	permission_classes = [IsAuthenticatedOrReadOnly]
 	queryset = Book.objects.all()
 	serializer_class = BookSerializer
+	filterset_fields = "__all__"
+	search_fields = ["title", "publication_year"]
+	ordeing_fields = ["title", "publication_year"]
 
 class DetailView(generics.RetrieveAPIView):
 	permission_classes = [IsAuthenticatedOrReadOnly]
