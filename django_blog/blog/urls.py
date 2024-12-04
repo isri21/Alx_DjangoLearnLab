4a9592b47b2387a2feae3f24dae093466b72a276
isri21 <israelalemu21@gmail.com>
2024-12-04 03:27:36 +0300

    Modify Task 1 #1

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -6,7 +6,7 @@ from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
 	path("home/", Register.as_view(), name="home"),
 	path("posts/", Register.as_view(), name="posts"),
-	path("login/", LoginView.as_view(), name="login"),
+	path("login/", LoginView.as_view(template_name="blog/login.html"), name="login"),
 	path("logout/", LogoutView.as_view(), name="logout"),
 	path("register/", Register.as_view(), name="register"),
 	path("profile/", profile, name="profile"),
