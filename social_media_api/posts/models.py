80531d16745df1f37b5f74d797bcf994e779e2cc
isri21 <israelalemu21@gmail.com>
2024-12-15 18:50:09 +0300

    Modified Task 1 #0

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -6,9 +6,9 @@ user = get_user_model()
 class Post(models.Model):
 	author = models.ForeignKey(user, on_delete=models.CASCADE, related_name="posts")
 	title = models.CharField(max_length=150)
-	content = models.TextField(max_length=500)
-	created_at = models.DateField(auto_now_add=True)
-	updated_at = models.DateField(auto_now=True)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.title
@@ -16,9 +16,9 @@ class Post(models.Model):
 class Comment(models.Model):
 	post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name="comments_post")
 	author = models.ForeignKey(user, on_delete=models.CASCADE, related_name="comments_author")
-	content = models.TextField(max_length=500)
-	created_at = models.DateField(auto_now_add=True)
-	updated_at = models.DateField(auto_now=True)
+	content = models.TextField()
+	created_at = models.DateTimeField(auto_now_add=True)
+	updated_at = models.DateTimeField(auto_now=True)
 
 	def __str__(self):
 		return self.content[:20]
--- social_media_api/posts/permissions.py
+++ social_media_api/posts/permissions.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import BasePermission
 
 class IsOwner(BasePermission):
 	def get_object_permission(self, request, view, obj):
-		if request.user == obj.author
+		if request.user == obj.author:
 			return True
 		else:
 			return False
